d44d27b33b7a126a5a0dc0b03fa8a2254ef6ff6e
Mohammad Arkan <mdarkanurl@gmail.com>
2025-08-08 19:34:28 +0600

    Undo databaes

---
+++ services/problems/prisma/migrations/20250808133401_undo_database/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isContestSubmission` on the `Submit` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Submit" DROP COLUMN "isContestSubmission";
