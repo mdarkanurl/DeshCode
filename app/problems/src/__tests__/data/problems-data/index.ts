258d8dd85e63c7a057d2401e46ef38a405521112
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-17 21:03:23 +0600

    Added unformattedTextJustification

--- app/problems/src/__tests__/data/problems-data/index.ts
+++ app/problems/src/__tests__/data/problems-data/index.ts
@@ -1,9 +1,11 @@
 import nQueens from './n-queens.json';
 import textJustification from './text-justification.json';
+import unformattedTextJustification from './text-justification-unformatted.json';
 
 const problemsData = {
     nQueens,
-    textJustification
+    textJustification,
+    unformattedTextJustification
 }
 
 export {
