004b99815c1aa59d276c2eb19bb078fd0e52de0f
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-18 14:17:46 +0600

    Remove express server from worker

--- app/problems/src/worker/index.ts
+++ app/problems/src/worker/index.ts
@@ -1,14 +1,12 @@
-import express from "express";
 import { consume } from "./queue/consume";
 import { ProblemsTypes } from "@prisma/client";
 import { config } from "dotenv";
 config();
-const app = express();
+
 let ProblemsType: ProblemsTypes;
 
-app.listen(process.env.WORKER_PORT || 4000, async () => {
-  console.log(`🚀 Server running on port ${process.env.WORKER_PORT || 4000}`);
+(async () => {
   for (ProblemsType in ProblemsTypes) {
     await consume(ProblemsType);
   }
-});
+})();
