7c19f0f87048e306d9cc0355149840a94569a36a
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-18 19:46:13 +0600

    Added 11 more problems

--- app/problems/src/__tests__/data/problems-data/index.ts
+++ app/problems/src/__tests__/data/problems-data/index.ts
@@ -1,11 +1,33 @@
 import nQueens from './n-queens.json';
 import textJustification from './text-justification.json';
+import addTwoIntegers from './add-two-integers.json';
+import binaryTreeMaximumPathSum from './binary-tree-maximum-path-sum.json';
+import complexNumberMultiplication from './complex-number-multiplication.json';
+import kthSmallestElementInABst from './kth-smallest-element-in-a-bst.json';
+import regularExpressionMatching from './regular-expression-matching.json';
+import trappingRainWater from './trapping-rain-water.json';
+import uniquePathsIi from './unique-paths-ii.json';
+import utf8Validation from './utf8-validation.json';
+import wildcardMatching44 from './wildcard-matching-44.json';
+import wordLadder127 from './word-ladder-127.json';
 import unformattedTextJustification from './text-justification-unformatted.json';
+import gameOfLife from './game-of-life.json';
 
 const problemsData = {
     nQueens,
     textJustification,
-    unformattedTextJustification
+    unformattedTextJustification,
+    addTwoIntegers,
+    binaryTreeMaximumPathSum,
+    complexNumberMultiplication,
+    kthSmallestElementInABst,
+    regularExpressionMatching,
+    trappingRainWater,
+    uniquePathsIi,
+    utf8Validation,
+    wildcardMatching44,
+    wordLadder127,
+    gameOfLife
 }
 
 export {
