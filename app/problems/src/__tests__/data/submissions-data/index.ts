d8bc7c687e40322fcc2e34d94a587886b2d2990a
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-19 15:50:20 +0600

    Added solution data file for more read and mangeable code

---
+++ app/problems/src/__tests__/data/submissions-data/index.ts
@@ -0,0 +1,43 @@
+import textJustificationSolution from "./text-justification.json";
+import invalidProblemIdSolution from "./invalid-problem-id.json";
+import invalidInputSolution from "./invalid-input.json";
+import languageNotSupportSolution from "./language-not-support.json";
+import invalidFunctionSignature from "./invalid-function-signature.json";
+import incorrectProblemSolution from "./incorrect-problem-solution.json";
+import failedProblemSolution from "./failed-problem-solution.json";
+import regularExpressionMatchingProblemSolution from "./regular-expression-matching-solution.json";
+import addTwoIntegersSolution from "./add-two-integers-solution.json";
+import binaryTreeMaximumPathSumSolution from "./binary-tree-maximum-path-sum-solution.json";
+import complexNumberMultiplicationSolutionSolution from "./complex-number-multiplication-solution.json";
+import gameOfLifeSolution from "./game-of-life-solution.json";
+import kthSmallestElementInABstSolution from "./kth-smallest-element-in-a-bst-solution.json";
+import trappingRainWaterSolution from "./trapping-rain-water-solution.json";
+import uniquePathsIiSolution from "./unique-paths-ii-solution.json";
+import utf8ValidationSolution from "./utf8-validation-solution.json";
+import wildcardMatching44Solution from "./wildcard-matching-44-solution.json";
+import wordLadder127Solution from "./word-ladder-127-solution.json";
+
+const solutionData = {
+    textJustificationSolution,
+    invalidProblemIdSolution,
+    invalidInputSolution,
+    languageNotSupportSolution,
+    invalidFunctionSignature,
+    incorrectProblemSolution,
+    regularExpressionMatchingProblemSolution,
+    failedProblemSolution,
+    addTwoIntegersSolution,
+    binaryTreeMaximumPathSumSolution,
+    complexNumberMultiplicationSolutionSolution,
+    gameOfLifeSolution,
+    kthSmallestElementInABstSolution,
+    trappingRainWaterSolution,
+    uniquePathsIiSolution,
+    utf8ValidationSolution,
+    wildcardMatching44Solution,
+    wordLadder127Solution
+}
+
+export {
+    solutionData
+}
