573bf4c5971a803ac217d06867a2a91a8209cd5a
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-20 15:25:55 +0600

    Added updated data for discussion

--- app/discussions/src/__tests__/data/discussions-data/index.ts
+++ app/discussions/src/__tests__/data/discussions-data/index.ts
@@ -1,11 +1,13 @@
 import invalidDiscussion from "./invalid-discussion.json";
 import correctDiscussion from "./correct-input-discussion.json";
 import correctDiscussionTwo from "./correct-input-discussion-two.json";
+import updatedDiscussion from "./updated-discussion.json";
 
 const discussionData = {
     invalidDiscussion,
     correctDiscussion,
-    correctDiscussionTwo
+    correctDiscussionTwo,
+    updatedDiscussion
 }
 
 export {
