0434f879787f364d10e0b0e0d87fb7700ae0247f
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-20 12:17:19 +0600

    Added some data for discussions test cases

---
+++ app/discussions/src/__tests__/data/discussions-data/index.ts
@@ -0,0 +1,13 @@
+import invalidDiscussion from "./invalid-discussion.json";
+import correctDiscussion from "./correct-input-discussion.json";
+import correctDiscussionTwo from "./correct-input-discussion-two.json";
+
+const discussionData = {
+    invalidDiscussion,
+    correctDiscussion,
+    correctDiscussionTwo
+}
+
+export {
+    discussionData
+}
