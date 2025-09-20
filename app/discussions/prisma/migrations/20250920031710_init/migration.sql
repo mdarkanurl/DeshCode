4b515675841f9c5a2681ba0d072a33c3f886ac7b
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-20 09:18:47 +0600

    Changed database env name

--- app/discussions/prisma/migrations/20250920031710_init/migration.sql
+++ app/discussions/prisma/migrations/20250920031710_init/migration.sql
@@ -1,23 +1,23 @@
 -- CreateEnum
-CREATE TYPE "Topic" AS ENUM ('Career', 'Compensation', 'Feedback', 'Interview');
+CREATE TYPE "public"."Topic" AS ENUM ('Career', 'Compensation', 'Feedback', 'Interview');
 
 -- CreateTable
-CREATE TABLE "Disscuss" (
+CREATE TABLE "public"."Discussions" (
     "id" SERIAL NOT NULL,
     "userId" TEXT NOT NULL,
-    "topic" "Topic" NOT NULL DEFAULT 'Career',
+    "topic" "public"."Topic" NOT NULL DEFAULT 'Career',
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Disscuss_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Discussions_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Comments" (
+CREATE TABLE "public"."Comments" (
     "id" SERIAL NOT NULL,
-    "discussId" INTEGER NOT NULL,
+    "discussionsId" INTEGER NOT NULL,
     "userId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -27,4 +27,4 @@ CREATE TABLE "Comments" (
 );
 
 -- AddForeignKey
-ALTER TABLE "Comments" ADD CONSTRAINT "Comments_discussId_fkey" FOREIGN KEY ("discussId") REFERENCES "Disscuss"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "public"."Comments" ADD CONSTRAINT "Comments_discussionsId_fkey" FOREIGN KEY ("discussionsId") REFERENCES "public"."Discussions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
