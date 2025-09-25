f953ce51f01c6902d7e577bafab3854a4ff505c2
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-25 14:32:06 +0600

    Added boilerplate describe block for /api/v1/contests and two test cases

--- app/contests/src/__tests__/index.test.ts
+++ app/contests/src/__tests__/index.test.ts
@@ -26,3 +26,76 @@ describe("App", () => {
     });
   });
 });
+
+// Describe block for /api/v1/contests
+describe("/api/v1/contests", () => {
+
+  let accessTokenCookie: string;
+  let refreshTokenCookie: string;
+  let accessTokenCookie2: string;
+  let refreshTokenCookie2: string;
+
+  beforeAll(async () => {
+
+    // Call Auth service to login as user
+    const loginRes = await request("http://localhost:3004") // or replace with Auth service URL
+      .post("/api/v1/auth/login")
+      .send({
+        email: "admin@DeshCode.com",
+        password: "testingPassword"
+    });
+
+    // loginRes.headers['set-cookie'] is an array of cookie strings
+    const cookies: string[] = loginRes.headers["set-cookie"] as any;
+
+    // Find the cookies by name
+    accessTokenCookie = cookies.find(c => c.startsWith("accessToken="))!;
+    refreshTokenCookie = cookies.find(c => c.startsWith("refreshToken="))!;
+  });
+
+  it("should return 401 unauthorized to create contests", async () => {
+    const res = await request(app).post("/api/v1/contests");
+
+    expect(res.status).toBe(401);
+    expect(res.body).toHaveProperty("Success", false);
+    expect(res.body).toHaveProperty("Message", "Unauthorized");
+    expect(res.body).toHaveProperty("Data", null);
+  });
+
+  it("should return 400 invalid input", async () => {
+    const res = await request(app).post("/api/v1/contests")
+        .set("Cookie", [accessTokenCookie, refreshTokenCookie])
+        .send({  });
+
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty("Success", false);
+    expect(res.body).toHaveProperty("Message", "Invalid input");
+    expect(res.body).toHaveProperty("Data", null);
+
+    expect(res.body.Errors[0].code).toBe("invalid_type");
+    expect(res.body.Errors[0].expected).toBe("string");
+    expect(res.body.Errors[0].received).toBe("undefined");
+    expect(res.body.Errors[0].path[0]).toBe("name");
+    expect(res.body.Errors[0].message).toBe("Required");
+
+    expect(res.body.Errors[1].code).toBe("invalid_type");
+    expect(res.body.Errors[1].expected).toBe("array");
+    expect(res.body.Errors[1].received).toBe("undefined");
+    expect(res.body.Errors[1].path[0]).toBe("problemsId");
+    expect(res.body.Errors[1].message).toBe("Required");
+
+    expect(res.body.Errors[2].code).toBe("invalid_type");
+    expect(res.body.Errors[2].expected).toBe("string");
+    expect(res.body.Errors[2].received).toBe("undefined");
+    expect(res.body.Errors[2].path[0]).toBe("startTime");
+    expect(res.body.Errors[2].message).toBe("Required");
+
+    expect(res.body.Errors[3].code).toBe("invalid_type");
+    expect(res.body.Errors[3].expected).toBe("string");
+    expect(res.body.Errors[3].received).toBe("undefined");
+    expect(res.body.Errors[3].path[0]).toBe("endTime");
+    expect(res.body.Errors[3].message).toBe("Required");
+  });
+
+
+});
