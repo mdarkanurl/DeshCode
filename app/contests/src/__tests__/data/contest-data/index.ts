312303008003804e13dc451bbb7072f126c70596
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-26 16:12:46 +0600

    Added four file for data of contests and index.ts file

---
+++ app/contests/src/__tests__/data/contest-data/index.ts
@@ -0,0 +1,15 @@
+import invalidStartTimeContestInput from "./invalid-start-time-contest-input.json";
+import invalidEndTimeContestInput from "./invalid-end-time-contest-input.json";
+import invalidStartTimeFormateContestInput from "./invalid-start-time-formate.json";
+import invalidEndTimeFormateContestInput from "./invaild-end-time-formate.json";
+
+const contestsInput = {
+    invalidStartTimeContestInput,
+    invalidEndTimeContestInput,
+    invalidStartTimeFormateContestInput,
+    invalidEndTimeFormateContestInput
+}
+
+export {
+    contestsInput
+}
