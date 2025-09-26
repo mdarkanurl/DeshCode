32dbdb866e2d10f2f5cf60fd8b7d15b55d5d55c3
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-26 16:18:12 +0600

    added correct input for contest

--- app/contests/src/__tests__/data/contest-data/index.ts
+++ app/contests/src/__tests__/data/contest-data/index.ts
@@ -2,12 +2,14 @@ import invalidStartTimeContestInput from "./invalid-start-time-contest-input.jso
 import invalidEndTimeContestInput from "./invalid-end-time-contest-input.json";
 import invalidStartTimeFormateContestInput from "./invalid-start-time-formate.json";
 import invalidEndTimeFormateContestInput from "./invaild-end-time-formate.json";
+import correctContestInput from "./correct-contest-input.json";
 
 const contestsInput = {
     invalidStartTimeContestInput,
     invalidEndTimeContestInput,
     invalidStartTimeFormateContestInput,
-    invalidEndTimeFormateContestInput
+    invalidEndTimeFormateContestInput,
+    correctContestInput
 }
 
 export {
