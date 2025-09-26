f592a18230be19da3b2522d6f305625f2466f4c8
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-26 17:26:44 +0600

    Remove some data

--- app/contests/src/__tests__/data/contest-data/index.ts
+++ app/contests/src/__tests__/data/contest-data/index.ts
@@ -1,14 +1,10 @@
 import invalidStartTimeContestInput from "./invalid-start-time-contest-input.json";
-import invalidEndTimeContestInput from "./invalid-end-time-contest-input.json";
 import invalidStartTimeFormateContestInput from "./invalid-start-time-formate.json";
-import invalidEndTimeFormateContestInput from "./invaild-end-time-formate.json";
 import correctContestInput from "./correct-contest-input.json";
 
 const contestsInput = {
     invalidStartTimeContestInput,
-    invalidEndTimeContestInput,
     invalidStartTimeFormateContestInput,
-    invalidEndTimeFormateContestInput,
     correctContestInput
 }
 
