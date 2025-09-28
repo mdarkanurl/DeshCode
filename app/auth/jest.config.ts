8b814b5611c389a727d690ad31f9965d30760aea
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-28 22:13:51 +0600

    Added file for test config

---
+++ app/auth/jest.config.ts
@@ -0,0 +1,18 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  globalSetup: "./src/__tests__/setup.ts",
+  globalTeardown: "./src/__tests__/teardown.ts",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/src/__tests__/**/*.test.(ts|js)', '**/?(*.)+(spec|test).(ts|js)'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  testTimeout: 60000
+};
+
+export default config;
