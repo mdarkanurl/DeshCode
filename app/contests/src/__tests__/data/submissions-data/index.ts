c46a13edb80ad991800b26d3db07f1b3a39b2529
Mohammad Arkan <mdarkanurl@gmail.com>
2025-09-28 21:29:48 +0600

    Added data for submission route test

--- app/contests/src/__tests__/data/submissions-data/index.ts
+++ app/contests/src/__tests__/data/submissions-data/index.ts
@@ -1,5 +1,9 @@
 import submissionInput from './submissions-input.json';
+import incorrectSubmissionSolution from './incorrect-submission-input.json';
+import invalidFunctionSubmissionInput from './invalid-function-submission-input.json';
 
 export {
-    submissionInput
+    submissionInput,
+    incorrectSubmissionSolution,
+    invalidFunctionSubmissionInput
 }
